78de6d62467295b486c3f44491038b6381dbb581
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-02-01 10:18:59 +0100

    essai

--- Test_son.py
+++ Test_son.py
@@ -1,2 +1,14 @@
-import os
-os.system("omxplayer /sound/voice_walle.mp3")
+import pygame
+
+# Initialisation de pygame
+pygame.mixer.init()
+
+# Charger le fichier MP3
+pygame.mixer.music.load("/sound/voice_walle.mp3")
+
+# Jouer le son
+pygame.mixer.music.play()
+
+# Attendre la fin du son
+while pygame.mixer.music.get_busy():
+    continue
