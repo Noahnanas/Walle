71c7c1c64aaa82a1f66649d75a35f8c52bb92fba
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-02-07 10:52:39 +0100

    liste deroulante

--- serveur_web/serv1.py
+++ serveur_web/serv1.py
@@ -3,14 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/slider', methods=['POST'])
-def slider():
-    valeur = request.form.get('valeur')
-    print(f"Valeur du slider reçue : {valeur}")
-    return "Valeur reçue"
+@app.route('/servo', methods=['POST'])
+def servo():
+    servo_id = request.form['servo']  # Récupère le servomoteur sélectionné
+    angle = request.form['angle']  # Récupère l'angle
+
+    print(f"Servo sélectionné : {servo_id}, Angle : {angle}")  # Affichage côté serveur
+
+    # Ici, tu peux ajouter du code pour contrôler le servomoteur avec GPIO
+    return "OK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
