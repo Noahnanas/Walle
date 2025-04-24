acaa5e84f3026f6a3cc5e23c1d0c72ee55136cd8
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-04-24 17:22:46 +0200

    init_socketio

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from Modes import Auto,Follow,Manual,Sequence,Sleep
 from Web import server
 from Mvt_walle import Walle
 from Modes_manager import ModeManager
-from Web.log_redirector import redirect_stdout
+from Web.log_redirector import redirect_stdout, init_socketio
 import threading
 import time
 
@@ -16,6 +16,7 @@ flask_thread = threading.Thread(target=server.run_web_server)
 flask_thread.daemon = True
 flask_thread.start()
 
+init_socketio(server.socketio)
 redirect_stdout()
 
 # modes
