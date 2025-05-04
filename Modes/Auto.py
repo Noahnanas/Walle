74c7c411f7229aa2be321c9f60a3ba702ed89c10
Mousseauliam <158599547+Mousseauliam@users.noreply.github.com>
2025-05-04 16:34:54 +0200

    head tilt

--- Modes/Auto.py
+++ Modes/Auto.py
@@ -3,7 +3,7 @@ from Vision.cam import get_head_factor
 
 active = False
 
-deadzone = 0.1
+deadzone = 0.08
 y_step = 0.02
 x_factor = 0.5
 tilt_factor = 1.0
@@ -26,12 +26,12 @@ def run(robot,server):
             neck_LR_temp = round((head_factor[0] - 0.5) * x_factor + 0.5,2)
             if (neck_LR!= neck_LR_temp):
                 robot.neckLR(neck_LR_temp)
-            """
+
             head_angle = robot.get_coef("head_angle")
             head_angle_temp = round((head_factor[2] - 0.5) * tilt_factor + 0.5,2)
             if (head_angle!= head_angle_temp):
                 robot.headAngle(head_angle_temp)
-            """
+
             time.sleep(0.1) 
 
 def stop():
